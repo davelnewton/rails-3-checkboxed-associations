fb79dffe3d504b42f271b9f7ba4da4a06ecd2462
Dave Newton <davelnewton@gmail.com>
2011-08-29 00:25:44 -0400

    Had all the attr_accessible stuff in the wrong model :(
    And now w/o it in *either* model everything still works?! Wth--I would have sworn it didn't in my actual code >:|

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :category_ids, :categories_attributes
   validates :name, :presence => true
   has_many :product_categories
   has_many :products, :through => :product_categories
