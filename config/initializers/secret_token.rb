b589e789dd9c8de113ef7047325f625e391bccee
Dave Newton <davelnewton@gmail.com>
2011-08-28 23:00:44 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3CheckboxedAssociations::Application.config.secret_token = '96fe9a2873b71ce9a21c7e20658953aa833f0b8a52c0de4a13cba80a28e1cf104b49ee5369f3db92beffbda341499137dfbf86759fb5b1e2689bcfeb3bb53321'
